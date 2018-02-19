91af1b03e8bd116c7626e778377b94c67c5fa037
Buvan <9099941+buva7687@users.noreply.github.com>
2018-02-18 21:12:51 -0800

    Update count.py

--- count.py
+++ count.py
@@ -1,22 +1,24 @@
 # To get the all csv files from the mentioned path and its sub directories
-import os, re, csv, time, itertools
-import math
+import os, re, csv, itertools
+import math, sys
 import pandas as pd
 
-#print(time.time())
-with open('Directorypath\\result.txt', 'w') as a:
-    for path , subdirs, files in os.walk('Data directorypath\\data\\'):
+#First identify the csv files in mentioned path. This result will be used in subsequent scripts.
+#Note:Path needs to be changed.
+with open('C:\\Users\\Buvaneswaran\\Downloads\\data\\result.txt', 'w') as a:
+    for path , subdirs, files in os.walk('C:\\Users\\Buvaneswaran\\data\\'):
         for filenames in files:
             if re.match("^.*.csv$", filenames):
                 f = os.path.join(path,filenames)
-                fn=f.rstrip('\r\n')
+                fn = f.rstrip('\r\n')
                 a.write(str(fn))
                 a.write('\n')
 a.close()
 
 paths = []
 
-with open("Directorypath\\result.txt")as file:
+
+with open("C:\\Users\\Buvaneswaran\\Downloads\\data\\result.txt")as file:
     filecount = 0
     for line in file:
         line = line.rstrip()
@@ -24,48 +26,52 @@ with open("Directorypath\\result.txt")as file:
         filecount = filecount + 1
     print("File count is ", filecount)
 
-    with open('Directorypath\\columns.txt', 'w') as cf:
+    # The below part is to identify the column count. The result will be stored in columns.txt
+    with open('C:\\Users\\Buvaneswaran\\Downloads\\data\\columns.txt', 'w') as cf:
         sumcolum = 0
         for singlefile in paths:
             #print(singlefile)
             exampleFile = open(singlefile, 'r', encoding="utf8", errors='ignore')
-            reader = csv.reader(exampleFile)
-            ncol = len(next(reader))  # Read first line and count columns
-            sumcolum = sumcolum + ncol
+            reader      = csv.reader(exampleFile)
+            ncol        = len(next(reader))  # Read first line and count columns
+            sumcolum    = sumcolum + ncol
             #print('File name is:', singlefile, ncol)
             cf.write(str(ncol))
             cf.write('\n')
             exampleFile.seek(0)
+    print("sum column",sumcolum)
+    print("filecount", filecount)
     print("Average no. of columns in all files:", math.ceil(sumcolum/filecount))
     cf.close()
 
-    with open('Directorypath\\rows.txt', 'w') as rf:
+    # The below part is to identify the word count. The result will be stored in words.txt
+    with open('C:\\Users\\Buvaneswaran\\Downloads\\data\\words.txt', 'w') as wf:
         for singlefile in paths:
-            exampleFile = open(singlefile, 'r', encoding="utf8", errors='ignore')
+            try:
+                exampleFile = open(singlefile, 'r', encoding="utf8", errors='ignore')
+                df = pd.read_csv(exampleFile,skip_blank_lines=True)
+                words_count = df.count().sum()
+                print(singlefile, words_count)
+                wf.write(str(singlefile))
+                wf.write(',')
+                wf.write(str(words_count))
+                wf.write('\n')
+            except (pd.errors.EmptyDataError,pd.errors.ParserError ) as e:
+                continue
 
-            df = pd.read_csv(exampleFile,error_bad_lines=False)
+    wf.close()
 
-            words_count = df.count().sum()
-            print(singlefile, words_count)
-
-            rf.write(str(singlefile))
-            #rf.write(str(words_count))
-            #Need to check how to populate , after filename?
-            rf.write(','.join(str(words_count)))
-            rf.write('\n')
-    rf.close()
-
-    with open('Directorypath\\Total.txt', 'w') as tf:
+    # The below part is to identify the total row count. The result will be stored in Total.txt
+    with open('C:\\Users\\Buvaneswaran\\Downloads\\data\\Total.txt', 'w') as tf:
         totalrowcount = 0
         for singlefile in paths:
             exampleFile = open(singlefile, 'r', encoding="utf8", errors='ignore')
-
             reader = csv.reader(exampleFile)
             row_count = sum(1 for row in reader)
             totalrowcount = totalrowcount + row_count
             #print(row_count)
-        print("Total row count is ", totalrowcount-filecount)
-            #tf.write('\n')
-    tf.close()
+        #print("file count is",filecount)
+        tf.write(str(totalrowcount - filecount))
+        print("Total row count is ", (totalrowcount - filecount)) # file count subtraction is to remove the headers
 
-print(time.time())
+    tf.close()
